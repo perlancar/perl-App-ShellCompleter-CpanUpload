7ed2a6d191a847304e2db30623e80673012bc5ab
perlancar (@pc-home) <perlancar@gmail.com>
2015-01-02 13:09:04 +0700

    Initial commit

---
+++ lib/App/ShellCompleter/CpanUpload.pm
@@ -0,0 +1,16 @@
+package App::ShellCompleter::CpanUpload;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Shell completion for cpan-upload
+
+=head1 SYNOPSIS
+
+See L<_cpan-upload> included in this distribution.
+
+
+=head1 SEE ALSO
+
+L<CPAN::Uploader>, L<cpan-upload>
